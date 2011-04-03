4f695246bed952dc81ee15e3aff7d8b18e239a6e
Oddbjorn Steffensen <oddbjorn@tricknology.org>
2011-04-03 17:12:11 +0200

    First working code. Sorta.

--- phodash.rb
+++ phodash.rb
@@ -1,15 +1,12 @@
 require 'sinatra'
 require 'flickraw-cached'
+require 'haml'
+require 'pp'
 
-FlickRaw.api_key="b172842dbd43230e877cdd6a715a3135"
-FlickRaw.shared_secret="8a380b2f28e30fe2"
+get '/style.css' do
+	sass :style
+end
 
 get '/' do
-  list = flickr.photos.getRecent
-  puts list
-#  id = list[0].id
-#  secret = list[0].secret
-#  info = flickr.photos.getInfo :photo_id => id, :secret => secret
-#  exif = flickr.photos.getExif :photo_id => id, :secret => secret
-#  p exif
+  haml :overview
 end
