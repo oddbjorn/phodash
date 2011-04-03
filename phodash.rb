684a6e727db1df54fd2c835c54710b89fd99408f
Oddbjorn Steffensen <oddbjorn@tricknology.org>
2011-04-03 11:14:33 +0200

    Added initial flickraw code.

--- phodash.rb
+++ phodash.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'flickraw-cached'
+
+FlickRaw.api_key="b172842dbd43230e877cdd6a715a3135"
+FlickRaw.shared_secret="8a380b2f28e30fe2"
 
 get '/' do
-  "Hello world!"
+  list = flickr.photos.getRecent
+  id = list[0].id
+  secret = list[0].secret
+  info = flickr.photos.getInfo :photo_id => id, :secret => secret
+  exif = flickr.photos.getExif :photo_id => id, :secret => secret
+  p exif
 end
